44dab784a4ea5460071bae1c027aa7a4dc298068
blockplocker <noahallklint@gmail.com>
2025-04-24 11:50:03 +0200

    Refactor Product page with product selection form

--- Skrot och korn Razor Pages/Pages/Product.cshtml.cs
+++ Skrot och korn Razor Pages/Pages/Product.cshtml.cs
@@ -5,8 +5,20 @@ namespace Skrot_och_korn_Razor_Pages.Pages
 {
     public class ProductModel : PageModel
     {
+        public int Price { get; set; }
+        [BindProperty]
+        public bool Vehicle1 { get; set; }
+        [BindProperty]
+        public bool Vehicle2 { get; set; }
+        [BindProperty]
+        public bool Vehicle3 { get; set; }
         public void OnGet()
         {
+            Price = 0;
+        }
+        public void OnPost()
+        {
+            Price = (Vehicle1 ? 200000 : 0) + (Vehicle2 ? 500000 : 0) + (Vehicle3 ? 700000 : 0);
         }
     }
 }
